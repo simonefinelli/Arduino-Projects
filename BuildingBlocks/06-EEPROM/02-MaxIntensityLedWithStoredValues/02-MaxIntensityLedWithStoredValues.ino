68e01609e38656e482991065f039197105c0006f
Simone Finelli <simonefinelli.work@gmail.com>
2024-03-26 21:50:09 +0100

    Fix: syntax errors

--- BuildingBlocks/06-EEPROM/02-MaxIntensityLedWithStoredValues/02-MaxIntensityLedWithStoredValues.ino
+++ BuildingBlocks/06-EEPROM/02-MaxIntensityLedWithStoredValues/02-MaxIntensityLedWithStoredValues.ino
@@ -1,8 +1,7 @@
 /**
  * The program turn-on/off a LED using a potentiometer.
  * The max intensity value of the led is read from EEPROM.
- * The value can be changed by using the user's input from
- * the Serial Monitor.
+ * The value can be changed by using the user's input from the Serial Monitor.
  */
 
 #include <EEPROM.h>
@@ -14,6 +13,7 @@
 unsigned long previous_time = 0;
 unsigned long curr_time = millis();
 unsigned max_led_brightness;
+int buffer;
 
 void setup() {
   // configure serial monitor
@@ -26,7 +26,7 @@ void setup() {
   
   // set led brighteness using stored default settigs
   max_led_brightness = EEPROM.read(0);
-  analogWrite(LED_PIN_1, led_brightness);
+  analogWrite(LED_PIN_1, max_led_brightness);
 }
 
 void loop()
@@ -34,20 +34,22 @@ void loop()
   // user input
   curr_time = millis();
   if ((curr_time - previous_time) > DELAY) {  // async user input check
-    if (Serial.available() > 0) { // check serial monitor data
+    if (Serial.available() > 0) { // check data from user
       buffer = Serial.parseInt();
-      if (buffer >= 125 && buffer <= 255) {
+      if (buffer >= 50 && buffer <= 255) {
         Serial.println("Max Intensity default value updated to: " + String(buffer));
-        EEPROM.write(0, buffer);  // store new default value into EEPROM att address 0x00
+        EEPROM.write(0, buffer);  // store new default value into EEPROM at address 0x00
+        max_led_brightness = buffer;  // for the current session
       } else {
-        Serial.println("Value must be in the range 125-255!");
+        Serial.println("Value must be in the range 50-255!");
       }
+    }
     previous_time += DELAY;
   }
   
-  // Potentimeter
+  // Potentiometer
   int led_brightness = int(analogRead(POTENTIOMETER_PIN) * 0.25);
-  if (led_brightness <= max_led_brightness
+  if (led_brightness <= max_led_brightness)
   	analogWrite(LED_PIN_1, led_brightness);  // 100 to 0% of 5v
   
   
