9b6a5fd7bd4289de1201753a3660c4d1e8e6a47e
Simone Finelli <simonefinelli.work@gmail.com>
2024-03-30 22:56:56 +0100

    Feat: add ultrasonic sensor description

---
+++ BuildingBlocks/07-ComplexComponents/UltrasonicSensor/01-UltrasonicSensor/01-UltrasonicSensor.ino
@@ -0,0 +1,26 @@
+/**
+ * A Ultrasonic Sensor can be used to detect the distance of an object.
+ * There are multiple ultrasonic sensor on the marketl. Here we'are utilizing the HC-SR04.
+ *
+ * The HC-SR04 has 4 pins:
+ *  - VCC: Power (5v);
+ *  - GND: Ground;
+ *  - TRIG: to trig the sensor (output mode);
+ *  - ECHO: receive the data (to get the distance).
+ * 
+ * ATTENTION: Echo pin must be connected to an Interrupt pin!
+ *
+ * Each sensor have its characteristics, for example this one can detect an object between 2cm to 400cm,
+ * with a angle of 15 degree.
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
