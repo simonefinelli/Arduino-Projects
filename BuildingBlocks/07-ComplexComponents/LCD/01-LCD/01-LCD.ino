65ebcc6056dc35d8083e3eb7a9ba779f0a6d9e21
Simone Finelli <simonefinelli.work@gmail.com>
2024-05-11 21:35:50 +0200

    Chore: init LCD section

---
+++ BuildingBlocks/07-ComplexComponents/LCD/01-LCD/01-LCD.ino
@@ -0,0 +1,16 @@
+/**
+ * In this lecture we will explore the LCD actuator (we do not call it as sensor
+ * because we can control its behaviour from Arduino).
+ *
+ * The LCD is an easy-to-use component. It can use to print at screen all sort of text.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
