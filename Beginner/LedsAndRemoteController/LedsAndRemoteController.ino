ea0fd3791ff26bbff4db110b92856d4c94a9ee71
Simone Finelli <simonefinelli.work@gmail.com>
2024-06-06 22:06:25 +0200

    Chore: add leds handling

--- Beginner/LedsAndRemoteController/LedsAndRemoteController.ino
+++ Beginner/LedsAndRemoteController/LedsAndRemoteController.ino
@@ -13,29 +13,18 @@
 #include <IRremote.hpp>
 
 #define IR_RECEIVE_PIN 5
+#define LED_1_PIN 10
+#define LED_2_PIN 11 
+#define LED_3_PIN 12
+
+unsigned led_status[3] = {0};  // handle on-off of leds
 
 enum IdBtns {
-  POWER = 0x45,
-  VOL_UP = 0x46,
   STOP = 0x47,
-  BACKWARD = 0x44,
-  PLAY = 0x40,
-  FORWARD = 0x43,
-  ARROW_DOWN = 0x7,
-  VOL_DOWN = 0x15,
-  ARROW_UP = 0x9,
   ZERO = 0x16,
-  EQ = 0x19,
-  REPT = 0xD,
   ONE = 0xC,
   TWO = 0x18,
-  THREE = 0x5E,
-  FOUR = 0x8,
-  FIVE = 0x1C,
-  SIX = 0x5A,
-  SEVEN = 0x42,
-  EIGHT = 0x52,
-  NINE = 0x4A
+  THREE = 0x5E
 };
 
 void setup() {
@@ -43,6 +32,10 @@ void setup() {
   Serial.begin(115200);
   // init the IR receiver
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  // init leds
+  pinMode(LED_1_PIN, OUTPUT);
+  pinMode(LED_2_PIN, OUTPUT);
+  pinMode(LED_3_PIN, OUTPUT);
 }
 
 void loop() {
@@ -51,74 +44,35 @@ void loop() {
     uint16_t command = IrReceiver.decodedIRData.command;
   
     switch (command) {
-      case POWER:
-        Serial.println("Pressed POWER button!");
-        break;
-      case VOL_UP:
-        Serial.println("Pressed VOL_UP button!");
-        break;
       case STOP:
         Serial.println("Pressed STOP button!");
         break;
-      case BACKWARD:
-        Serial.println("Pressed BACKWARD button!");
-        break;
-      case PLAY:
-        Serial.println("Pressed PLAY button!");
-        break;
-      case FORWARD:
-        Serial.println("Pressed FORWARD button!");
-        break;
-      case ARROW_DOWN:
-        Serial.println("Pressed ARROW_DOWN button!");
-        break;
-      case VOL_DOWN:
-        Serial.println("Pressed VOL_DOWN button!");
-        break;
-      case ARROW_UP:
-        Serial.println("Pressed ARROW_UP button!");
-        break;
       case ZERO:
         Serial.println("Pressed ZERO button!");
-        break;
-      case EQ:
-        Serial.println("Pressed EQ button!");
-        break;
-      case REPT:
-        Serial.println("Pressed REPT button!");
+        led_status[0] = LOW;
+        led_status[1] = LOW;
+        led_status[2] = LOW;
         break;
       case ONE:
         Serial.println("Pressed ONE button!");
+        led_status[0] = HIGH;
         break;
       case TWO:
         Serial.println("Pressed TWO button!");
+        led_status[1] = HIGH;
         break;
       case THREE:
         Serial.println("Pressed THREE button!");
-        break;
-      case FOUR:
-        Serial.println("Pressed FOUR button!");
-        break;
-      case FIVE:
-        Serial.println("Pressed FIVE button!");
-        break;
-      case SIX:
-        Serial.println("Pressed SIX button!");
-        break;
-      case SEVEN:
-        Serial.println("Pressed SEVEN button!");
-        break;
-      case EIGHT:
-        Serial.println("Pressed EIGHT button!");
-        break;
-      case NINE:
-        Serial.println("Pressed NINE button!");
+        led_status[2] = HIGH;
         break;
       default:
         Serial.println("Invalid data!");
     }
-
     IrReceiver.resume(); // enable receiving of the next value
   }
 
+  // update leds status
+  digitalWrite(LED_1_PIN, led_status[0]);
+  digitalWrite(LED_2_PIN, led_status[1]);
+  digitalWrite(LED_3_PIN, led_status[2]);
 }
