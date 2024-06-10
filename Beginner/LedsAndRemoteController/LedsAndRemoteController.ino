56166b6de4c5ef836eeae60670026acb89618eff
Simone Finelli <simonefinelli.work@gmail.com>
2024-06-10 21:14:53 +0200

    Chore: add lcd screen messages

--- Beginner/LedsAndRemoteController/LedsAndRemoteController.ino
+++ Beginner/LedsAndRemoteController/LedsAndRemoteController.ino
@@ -11,14 +11,16 @@
  */
 
 #include <IRremote.hpp>
+#include <LiquidCrystal.h>
 
-#define IR_RECEIVE_PIN 5
+/// LEDs config
 #define LED_1_PIN 10
 #define LED_2_PIN 11 
 #define LED_3_PIN 12
-
 unsigned led_status[3] = {0};  // handle on-off of leds
 
+/// IR remote controller config
+#define IR_RECEIVE_PIN 5
 enum IdBtns {
   STOP = 0x47,
   ZERO = 0x16,
@@ -26,6 +28,30 @@ enum IdBtns {
   TWO = 0x18,
   THREE = 0x5E
 };
+
+/// LCD config
+#define LCD_RS_PIN A5
+#define LCD_E_PIN A4
+#define LCD_D4_PIN 6
+#define LCD_D5_PIN 7
+#define LCD_D6_PIN 8
+#define LCD_D7_PIN 9
+
+// the size of the screen is expressed like a matrix
+#define LCD_WIDTH 16
+#define LCD_HEIGHT 2
+
+// init LCD display
+LiquidCrystal lcd(
+  LCD_RS_PIN,
+  LCD_E_PIN,
+  LCD_D4_PIN,
+  LCD_D5_PIN,
+  LCD_D6_PIN,
+  LCD_D7_PIN
+);
+
+char formatted_str[17];  // formatted string to show distance on lcd
 
 void setup() {
   // init serial communication
@@ -36,6 +62,9 @@ void setup() {
   pinMode(LED_1_PIN, OUTPUT);
   pinMode(LED_2_PIN, OUTPUT);
   pinMode(LED_3_PIN, OUTPUT);
+  // init LCD
+  lcd.begin(LCD_WIDTH, LCD_HEIGHT);
+  lcd.setCursor(0, 0);
 }
 
 void loop() {
@@ -45,30 +74,39 @@ void loop() {
   
     switch (command) {
       case STOP:
-        Serial.println("Pressed STOP button!");
+        Serial.println("Pressed button STOP!");
+        memset(formatted_str, ' ', 17);
         break;
       case ZERO:
-        Serial.println("Pressed ZERO button!");
+        Serial.println("Pressed button ZERO!");
         led_status[0] = LOW;
         led_status[1] = LOW;
         led_status[2] = LOW;
+        sprintf(formatted_str, "Turnoff all LEDs");
         break;
       case ONE:
-        Serial.println("Pressed ONE button!");
-        led_status[0] = HIGH;
+        Serial.println("Pressed button ONE!");
+        led_status[0] = led_status[0] == HIGH ? LOW : HIGH;
+        sprintf(formatted_str, "Toggle LED 1!");
         break;
       case TWO:
-        Serial.println("Pressed TWO button!");
-        led_status[1] = HIGH;
+        Serial.println("Pressed button TWO!");
+        led_status[1] = led_status[1] == HIGH ? LOW : HIGH;
+        sprintf(formatted_str, "Toggle LED 2!");
         break;
       case THREE:
-        Serial.println("Pressed THREE button!");
-        led_status[2] = HIGH;
+        Serial.println("Pressed button THREE!");
+        led_status[2] = led_status[2] == HIGH ? LOW : HIGH;
+        sprintf(formatted_str, "Toggle LED 3!");
         break;
       default:
         Serial.println("Invalid data!");
     }
-    IrReceiver.resume(); // enable receiving of the next value
+    // enable receiving of the next value
+    IrReceiver.resume();
+    // update lcd
+    lcd.print(formatted_str);
+    lcd.setCursor(0, 0);
   }
 
   // update leds status
